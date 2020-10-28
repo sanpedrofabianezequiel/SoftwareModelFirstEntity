9b870e7a7d96555357acf3867059e5565f944034
sanpedrofabianezequiel@gmail.com <sanpedrofabianezequiel@gmail.com>
2020-10-27 22:50:50 -0300

    Metodos para el ABM

--- FrontEndWPF/MainWindow.xaml.cs
+++ FrontEndWPF/MainWindow.xaml.cs
@@ -22,12 +22,15 @@ namespace FrontEndWPF
     {
         LogicaBI.Genero bi = new LogicaBI.Genero();
         public MainWindow()
-        { 
+        {
             InitializeComponent();
         }
 
         //Creamos el LOAD
+        public void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
 
+        }
         public void CargaGrilla()
         {
             List<Entidades.Genero> listaGeneros = bi.TraerTodos();
@@ -35,7 +38,34 @@ namespace FrontEndWPF
             dgGeneros.Columns[0].Width = 50;//id
             dgGeneros.Columns[1].Width = 150;//Nombre
             dgGeneros.Columns[2].Visibility = Visibility.Hidden;
+
+        }
+        //Botones
+        private void btnNuevo_Click(object sender, RoutedEventArgs e) 
+        {
+            Entidades.Genero nuevoGenero = new Entidades.Genero();
+            nuevoGenero.Nombre = this.txtNombre.Text;
+            bi.Agregar(nuevoGenero);
+            MessageBox.Show("Genero Agregado");
+            dgGeneros.DataContext = null;//Lo nuliamos y lo volvemos a llamar
+            CargaGrilla();
+            
         
         }
+
+        private void btnModificar_Click(object sender, RoutedEventArgs e)
+        {
+            Entidades.Genero nuevoGenero = new Entidades.Genero();
+            nuevoGenero.Id = Convert.ToInt32(this.txtID.Text);
+            nuevoGenero.Nombre = this.txtNombre.Text;
+            bi.Modificar(nuevoGenero);
+            MessageBox.Show("Genero Modificado");
+            dgGeneros.DataContext = null;//Lo nuliamos y lo volvemos a llamar
+            CargaGrilla();
+
+
+        }
+
+
     }
 }
